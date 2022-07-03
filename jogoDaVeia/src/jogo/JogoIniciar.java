425107386e03876c9e0be1c5c3e4c7048e07f369
Eduardo Moretto <edumoretto21051@gmail.com>
2022-07-03 08:17:36 -0300

    CM - Jogo da Velha

---
+++ jogoDaVeia/src/jogo/JogoIniciar.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jogo;
+
+/**
+ *
+ * @author eduar
+ */
+public class JogoIniciar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Jogo Frame = new Jogo();
+        Frame.setVisible(true);
+    }
+    
+}
